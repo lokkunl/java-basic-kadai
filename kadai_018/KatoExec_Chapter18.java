4e7df2e807b18d1102374e0281c42fcbf8d588cf
lokkunl <bb.5590.okkun@gmail.com>
2024-02-07 17:05:05 +0900

    kadai_018

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,22 @@
+package text.kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		
+		taro.setGivenName();
+		taro.execIntroduce();
+		
+		hanako.setGivenName();
+		hanako.execIntroduce();
+		
+		ichiro.setGivenName();
+		ichiro.execIntroduce();
+		
+
+	}
+
+}
